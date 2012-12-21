f6bf0fafb31fafd7c9713c7add139cd28e714505
cold night <wh_linux@126.com>
2012-12-21 10:56:17 +0800

    update settings.py

--- settings.py
+++ settings.py
@@ -3,28 +3,38 @@
 """
 # 设置
 """
-
+import os
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 ADMINS = ['coldnight.linux@gmail.com']
 
-USER = ''
+USER = 'pythonerclub@gmail.com'
 
-PASSWORD = ""
+PASSWORD = ''
 
-DEBUG = False
+DEBUG = True
 
-IMPORT = True     # 如果数据库中有而角色中无则添加好友
+IMPORT = False # 如果数据库中有而角色中无则添加好友
 
-__version__ = '0.1.5 alpha-threading'
+__version__ = '0.1.6 alpha-threading'
 
 PIDPATH = r'logs/clubot.pid'
 
 LOGPATH = r'logs/clubot.log'
 
-DB_NAME="group.db"
+DB_PATH= os.path.join(os.path.dirname(__file__), 'plugin/group.db')
+
+DB_HOST = 'localhost'
+
+DB_PORT = 3306
+
+DB_NAME = 'clubot'
+
+DB_USER = 'root'
+
+DB_PASSWD = ''
 
 status = u"Pythoner Club Linux/Vim/Python 技术交流"
 
