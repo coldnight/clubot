af13c1731ccc24a2ccf64a9b022dcd2d2e99686c
wh <wh_linux@126.com>
2013-08-28 17:42:31 +0800

    fix

--- settings.py
+++ settings.py
@@ -23,6 +23,8 @@ DB_NAME = 'clubot'
 DB_USER = 'root'
 DB_PASSWD = ''
 
+GFS_NAME = "clubot_gfs"
+
 STATUS = u"Pythoner Club Linux/Vim/Python 技术交流"
 MODES  = dict(talk = u'聊天模式', quiet = u'安静模式(不接收消息)')
 ADMINS = ['coldnight.linux@gmail.com']
