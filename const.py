206190874dd8c059dbd970091d74ab41ccf3247d
wh <wh_linux@126.com>
2013-09-04 10:35:11 +0800

    fix

--- const.py
+++ const.py
@@ -18,13 +18,3 @@ ONLINE = 1
 INFO_CHANGE_NICK_TIMES = "change_nick_times"
 INFO_RP = "rp"
 INFO_RP_DATE = "rp_date"
-
-MEDAL_DAY_RP = "当日人品之最"
-MEDAL_WEEK_RP = "周人品冠军"
-MEDAL_MONTH_RP ="月度人品冠军"
-
-MEDAL_DAY_RP_LOW = "当日衰神"
-
-MEDAL_ACTIVE = "活跃之王"
-MEDAL_SILENCE = "沉默之王"
-
